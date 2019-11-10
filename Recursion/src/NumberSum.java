acad6fd20a3207b2e20448c9d020dbf26a2f3b7f
McGuire <McGuire@MU-C1-TeacherB.mullen.pvt.k12.co.us>
2019-11-10 10:03:16 -0700

    fixed merge conflicts

--- Recursion/src/NumberSum.java
+++ Recursion/src/NumberSum.java
@@ -4,11 +4,7 @@ public class NumberSum
 
 	public static void main(String[] args)
 		{
-<<<<<<< HEAD
-		System.out.println(sumNumbers(5));
-=======
 		System.out.println(sumNumbers(7));
->>>>>>> branch 'master' of https://github.com/mrmikemcguire/Recursion.git
 		}
 	
 	public static int sumNumbers(int n)
